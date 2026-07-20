4bd3fcefa681c106138f029824fd45337e6d4cc5
bougueil <rmariana@gmail.com>
2026-07-20 13:36:13 +0200

    initial commit

---
+++ mix.exs
@@ -0,0 +1,40 @@
+defmodule StrFmt.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :str_fmt,
+      version: "0.1.0",
+      elixir: "~> 1.20",
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.40", only: :docs}
+    ]
+  end
+
+  defp aliases do
+    [
+      precommit: [
+        "compile --warnings-as-errors",
+        "deps.unlock --unused",
+        "format",
+        "test",
+        "credo"
+      ]
+    ]
+  end
+end
